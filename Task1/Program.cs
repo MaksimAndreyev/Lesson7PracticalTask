243a1f2d711e8674ac34680cc52fa820cf608e1f
maximandreev <maxands@yandex.ru>
2022-08-26 14:22:44 +0300

    Task 1 completed

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите число m:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число n:");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double[m, n];
+for (int i=0; i<m; i++)
+{
+    for (int j=0; j<n; j++)
+    {
+        array[i, j] = new Random().NextDouble() + new Random().Next(1,10);
+    }
+}
+for (int i=0; i<m; i++)
+{
+    for (int j=0; j<n; j++)
+    {
+        Console.Write(Math.Round(array[i, j], 2)+" ");
+    }
+    Console.WriteLine();
+}
